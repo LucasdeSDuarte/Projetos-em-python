2848e67c27ef840f389d79cf1fb736cad073257d
Lucas Duarte <111545060+LucasdeSDuarte@users.noreply.github.com>
2022-08-20 15:36:30 -0300

    Update and rename 02 -FINANCIAMENTO CASA.py to BASE-FINANCIAMENTO-RENDA-Imovel.py

--- PROJETOS EM PYTHON/BASE-FINANCIAMENTO-RENDA-Imovel.py
+++ PROJETOS EM PYTHON/BASE-FINANCIAMENTO-RENDA-Imovel.py
@@ -1,9 +1,9 @@
-casa = float(input('Qual é o valor da casa em reais: R$ '))
+imovel = float(input('Qual é o valor do imovél em reais: R$ '))
 salario = float(input('Qual é a sua renda mensal: R$ '))
 anos = int(input('Quantas vezes você pretende parcelar: '))
-parcelas = casa / (anos * 12)
+parcelas = imovel / (anos * 12)
 minimo = salario * 30 / 100
-print('O valor da casa é {:.2f}R$, em {} anos'.format(casa, anos))
+print('O valor da casa é {:.2f}R$, em {} anos'.format(imovel, anos))
 print('a prestação sera de {:.2f}R$'.format(parcelas))
 if parcelas <= minimo:
     print('O Seu financiamento será aprovado!')
